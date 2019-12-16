3bb96b5fcc2232babc83fd428baee6c99728211a
Vanessa Mae Verzosa <vtverzosa@student.apc.edu.ph>
2019-12-16 03:09:15 +0000

    2nd Commit for tryPONG.c

--- tryPONG.c
+++ tryPONG.c
@@ -1,1 +1,10 @@
-...
+#include <ncurses.h>
+typedef struct {short int x,y,c ; bool movhor, movver;} object;
+
+main () {
+	object scr; int i = 0, cont = 0; bool end = false;
+	initscr();
+	start_color();
+	init_pair(1, COLOR_YELLOW, COLOR_WHITE);
+
+}
