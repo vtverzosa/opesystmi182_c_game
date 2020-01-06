0e07c2a2cee1fa0131e50ae3ed73a6de7fb52303
Vanessa Mae Verzosa <vtverzosa@student.apc.edu.ph>
2020-01-06 03:18:56 +0000

    changed the whole program

--- tryPONG.c
+++ tryPONG.c
@@ -1,67 +1,115 @@
 #include <ncurses.h>
-typedef struct {short int x,y,c ; bool movhor, movver;} object;
+#include <string.h>
 
-main () {
-	object scr; int i = 0, cont = 0; bool end = false;
-	initscr();
+int main () {
+    // struct object
+    bool movhor;
+    bool movver;
+
+    // scr object
+    int i = 0;
+    int cont = 0;
+    bool end = FALSE;
+
+    int screeny = 0; // initialization of screen height
+    int screenx = 0; // initialization of screen width
+
+	initscr(); // initialize screen
+    cbreak();
+    noecho();
 	start_color();
-	init_pair(1, COLOR_YELLOW, COLOR_PINK);
-	keypad(stdscr,true);
-  	noecho();
-  	curs_set(0);
-  	getmaxyx(stdscr,scr.y,scr.x);
-  	object b1={scr.x-2,scr.y/2,0,false,false},b2={1,scr.y/2,0,false,false},b={scr.x/2,scr.y/2,0,false,false};
-  	mvprintw(4,0,"\t           oooooooooo                                        \n"
-               	     "\t           888    888  ooooooo    ooooooo    oooooooo8       \n"
-               	     "\t           888oooo88 888     888 888   888  888    88o       \n"
-                     "\t           888       888     888 888   888   888oo888o       \n"
-                     "\t          o888o        88ooo88  o888o o888o 888     888      \n"
-                     "\t                                             888ooo888     \n\n"
-               "\t Any questions please send me at vicente.bolea@gmail.com     \n"
-               "\t \t\t\tPlayer 1 your controls are 'a' and 'q'                \n"
-               "\t \t\t\tPlayer 2 your controls are the arrows of the keyboard \n"
-               "\t \t\t\tPush ANY key to start, 'p' for pause and ESC to quit" ); 
-  getch();
+	init_pair(1, COLOR_YELLOW, COLOR_MAGENTA);
+	keypad(stdscr, TRUE); // turn on arrow keys
+    curs_set(0); // hide cursor indicator
+
+    getmaxyx(stdscr, screeny, screenx); // declare screen height and width
+
+    // b object
+    int bx = screenx/2;
+    int by = screeny/2;
+    int bc = 0;
+    int bxb = FALSE;
+    int byb = FALSE;
+
+    // b1 object
+    int b1x = screenx - 2;
+    int b1y = screeny/2;
+    int b1c = 0;
+    int b1xb = FALSE;
+    int b1yb = FALSE;
+
+    // b2 object
+    int b2x = 1;
+    int b2y = screeny/2;
+    int b2c = 0;
+    int b2xb = FALSE;
+    int b2yb = FALSE;
+
+    mvprintw(4, (screenx + strlen(" oooooooooo                                  "))/2, " oooooooooo                                  ");
+    mvprintw(5, (screenx + strlen(" 888    888  ooooooo    ooooooo    oooooooo8 "))/2, " 888    888  ooooooo    ooooooo    oooooooo8 ");
+    mvprintw(6, (screenx + strlen(" 888oooo88 888     888 888   888  888    88o "))/2, " 888oooo88 888     888 888   888  888    88o ");
+    mvprintw(7, (screenx + strlen(" 888       888     888 888   888   888oo888o "))/2, " 888       888     888 888   888   888oo888o ");
+    mvprintw(8, (screenx + strlen(" 888       888     888 888   888   888       "))/2, " 888       888     888 888   888   888oo888o ");
+    mvprintw(9, (screenx + strlen(" 888       888     888 888   888   888oo888o "))/2, " 888       888     888 888   888   888oo888o ");
+    mvprintw(10, (screenx + strlen("o888o       88ooo88  o888o o888o 888     888"))/2, "o888o        88ooo88  o888o o888o 888     888");
+    mvprintw(11, (screenx + strlen("                                   888ooo888 "))/2, "                                   888ooo888 ");
+
+    mvprintw(13, (screenx + strlen("Any questions please send me at vtverzosa@student.apc.edu.ph"))/2, "Any questions please send me at vtverzosa@student.apc.edu.ph");
+    mvprintw(14, (screenx + strlen("Controls for Player 1 are 'w' and 's'"))/2, "Controls for Player 1 are 'w' and 's'");
+    mvprintw(15, (screenx + strlen("Controls for Player 2 are arrow keys"))/2, "Controls for Player 2 are arrow keys");
+    mvprintw(16, (screenx + strlen("Push ANY key to start, 'p' for pause and ESC to quit"))/2, "Push ANY key to start, 'p' for pause and ESC to quit");
+
+    getch();
 
 
-        if ((b.y==b1.y-1)||(b.y==b2.y-1)) {
-          b.movver=false;
-        } else if ((b.y==b1.y+1)||(b.y==b2.y+1)) {
-          b.movver=true;
-        } else if ((b.y != b1.y) && (b.y != b2.y)) {
-          (b.x>=scr.x-2) ? b1.c++: b2.c++;
-          b.x=scr.x/2;
-          b.y=scr.y/2;
-        }
-      }
-      b.x=b.movhor ? b.x+1 : b.x-1;
-      b.y=b.movver ? b.y+1 : b.y-1;
-
-      if (b1.y<=1)
-        b1.y=scr.y-2;
-      if (b1.y>=scr.y-1)
-        b1.y=2;
-      if (b2.y<=1)
-        b2.y=scr.y-2;
-      if (b2.y>=scr.y-1)
-        b2.y=2;
+    if ((by == b1y - 1) || (by == b2y - 1)) {
+        movver = FALSE;
+    } else if ((by == b1y + 1) || (by == b2y + 1)) {
+        movver = TRUE;
+    } else if ((by != b1y) && (by != b2y)) {
+        (bx >= screenx - 2) ? b1c++ : b2c++;
+        bx = screenx/2;
+        by = screeny/2;
     }
+
+    bx == movhor ? bx++ : bx--;
+    by == movver ? by++ : by--;
+
+    if (b1y <= 1)
+        b1y = screeny - 2;
+    if (b1y >= screeny - 1)
+        b1y = 2;
+    if (b2y <= 1)
+        b2y = screeny - 2;
+    if (b2y >= screeny - 1)
+        b2y = 2;
+
+
     switch (getch()) {
-      case KEY_DOWN: b1.y++; break;
-      case KEY_UP:   b1.y--; break;
-      case 'w':      b2.y--; break;
-      case 's':      b2.y++; break;
-      case 'p':      getchar(); break;
-      case 0x1B:    endwin(); end++; break;
+        case KEY_DOWN: b1y++; break;
+        case KEY_UP:   b1y--; break;
+        case 'w':      b2y--; break;
+        case 's':      b2y++; break;
+        case 'p':      getchar(); break;
+        case 0x1B:    endwin(); end++; break;
     }
+
     erase();
-    mvprintw(2,scr.x/2-2,"%i | %i",b1.c,b2.c);
-    mvvline(0,scr.x/2,ACS_VLINE,scr.y);
+
+    mvprintw(2, screenx / 2 - 2, "%i | %i", b1c, b2c);
+
+    mvvline(0, screenx / 2, ACS_VLINE, screeny);
     attron(COLOR_PAIR(1));
-    mvprintw(b.y,b.x,"O");
-    for(i=-1;i<2;i++){
-      mvprintw(b1.y+i,b1.x,"|");
-      mvprintw(b2.y+i,b2.x,"|");}
+
+    mvprintw(by, bx, "O");
+
+    for(i = -1; i < 2; i++) {
+        mvprintw(b1y + i, b1x, "|");
+        mvprintw(b2y + i, b2x, "|");
+    }
+
     attroff(COLOR_PAIR(2));
-  }
+
+    endwin();
+    return 0;
 }
